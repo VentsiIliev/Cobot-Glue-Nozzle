c901557834be759883de26308af18b26878d21dc
Ventsislav Iliev <ventsislav.iliew@gmail.com>
2025-04-13 07:23:48 +0300

    No change

--- runPlGui.py
+++ runPlGui.py
@@ -1,7 +1,6 @@
 from API.shared.settings.conreateSettings.CameraSettings import CameraSettings
 from API.shared.settings.conreateSettings.RobotSettings import RobotSettings
-from pl_gui.PlGui import  PlGui
-
+from pl_gui.PlGui import PlGui
 
 GET_SETTINGS_TOPIC = "settings/get"
 START_TOPIC = "start"
@@ -13,27 +12,27 @@ SAVE_ROBOT_CALIBRATION_POINT = "robot/control/manual/save"
 JOG_ROBOT_TOPIC = "robot/control/manual/jog/"
 UPDATE_CAMERA_FEED = "cameraFeed/update"
 
+
 class MockController():
     def init(self):
         pass
 
-    def sendRequest(self,message):
-
+    def sendRequest(self, message):
         if message == GET_SETTINGS_TOPIC:
             return self.__getSettings()
 
-        print("Sending Request: ",message)
-        return True,{"image":"MockImage",
-                     "height" : 4}
+        print("Sending Request: ", message)
+        return True, {"image": "MockImage",
+                      "height": 4}
 
     def updateCameraFeed(self):
         pass
 
-    def saveWorkpiece(self,data):
-        print("Saving WP: ",data)
+    def saveWorkpiece(self, data):
+        print("Saving WP: ", data)
 
-    def sendJogRequest(self,request):
-        print("Sending Jog Request: ",request)
+    def sendJogRequest(self, request):
+        print("Sending Jog Request: ", request)
 
     def saveRobotCalibrationPoint(self):
         print("Saving Robot Point")
@@ -41,7 +40,8 @@ class MockController():
     def __getSettings(self):
         cameraSettings = CameraSettings()
         robotSettings = RobotSettings()
-        return cameraSettings,robotSettings
+        return cameraSettings, robotSettings
+
 
 if __name__ == "__main__":
     mockController = MockController()
